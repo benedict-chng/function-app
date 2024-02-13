04ddc3831c198a3ca31c00f4ae6c5f7b4c1fdf6d
Benedict Chng <benedict.chng@coinvest.com.au>
2024-02-13 16:39:49 +1100

    Fixed hardcoding of connection string for SendToMessageBus function.

--- src/main/java/au/com/coinvest/function/ReceiveFromMessageBus.java
+++ src/main/java/au/com/coinvest/function/ReceiveFromMessageBus.java
@@ -11,10 +11,10 @@ public class ReceiveFromMessageBus {
      * This function will be invoked when a new message is received at the specified path. The message contents are provided as input to this function.
      */
     @FunctionName("ReceiveFromMessageBus")
-    public void run(
-        @QueueTrigger(name = "message", queueName = "queue1", connection = "benedictba9b_STORAGE") String message,
+    public void serviceBusProcess(
+        @ServiceBusQueueTrigger(name = "message", queueName = "queue1", connection = "AzureWebJobsStorage") String message,
         final ExecutionContext context
     ) {
-        context.getLogger().info("Java Queue trigger function processed a message: " + message);
+        context.getLogger().info(message);
     }
 }
--- src/main/java/au/com/coinvest/function/SendToMessageBus.java
+++ src/main/java/au/com/coinvest/function/SendToMessageBus.java
@@ -1,12 +1,17 @@
 package au.com.coinvest.function;
 
-import java.util.*;
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.servicebus.Message;
-import com.microsoft.azure.servicebus.QueueClient;
-import com.microsoft.azure.servicebus.ReceiveMode;
-import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
-import com.microsoft.azure.functions.*;
+import java.util.Optional;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.microsoft.azure.functions.annotation.ServiceBusQueueOutput;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -17,28 +22,49 @@ public class SendToMessageBus {
      * 1. curl -d "HTTP Body" {your host}/api/SendToMessageBus
      * 2. curl {your host}/api/SendToMessageBus?name=HTTP%20Query
      */
+    // @FunctionName("SendToMessageBus")
+    // public HttpResponseMessage run(
+    //     @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+    //     final ExecutionContext context
+    // ) {
+    //     String message = request.getBody().orElse("Default Message");
+    //     context.getLogger().info("Received message: " + message);
+
+    //     // Define your Service Bus namespace and queue name
+    //     String namespaceConnectionString = "Endpoint=sb://leyland.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AncIIyUfYg8pvFMF5fiZdZq+IShe7GwnL+ASbPeVzT4=";
+    //     String queueName = "queue1";
+
+    //     // Send the message to Azure Service Bus
+    //     try {
+    //         QueueClient queueClient = new QueueClient(new ConnectionStringBuilder(namespaceConnectionString, queueName), ReceiveMode.PEEKLOCK);
+    //         queueClient.send(new Message(message));
+    //         queueClient.close();
+    //     } catch (Exception e) {
+    //         context.getLogger().severe("Exception occurred: " + e.getMessage());
+    //         return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send message to Service Bus.").build();
+    //     }
+
+    //     return request.createResponseBuilder(HttpStatus.OK).body("Message sent to Service Bus.").build();
+    // }
+
     @FunctionName("SendToMessageBus")
-    public HttpResponseMessage run(
-        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+    public HttpResponseMessage pushToQueue(
+        @HttpTrigger(name = "request", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+        @ServiceBusQueueOutput(name = "message", queueName = "queue1", connection = "AzureWebJobsStorage") final OutputBinding<String> message,
         final ExecutionContext context
     ) {
-        String message = request.getBody().orElse("Default Message");
-        context.getLogger().info("Received message: " + message);
+        context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Define your Service Bus namespace and queue name
-        String namespaceConnectionString = "Endpoint=sb://leyland.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=AncIIyUfYg8pvFMF5fiZdZq+IShe7GwnL+ASbPeVzT4=";
-        String queueName = "queue1";
+        final String query = request.getQueryParameters().get("message");
+        final String content = request.getBody().orElse(query);
 
-        // Send the message to Azure Service Bus
-        try {
-            QueueClient queueClient = new QueueClient(new ConnectionStringBuilder(namespaceConnectionString, queueName), ReceiveMode.PEEKLOCK);
-            queueClient.send(new Message(message));
-            queueClient.close();
-        } catch (Exception e) {
-            context.getLogger().severe("Exception occurred: " + e.getMessage());
-            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send message to Service Bus.").build();
+        if (content == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a message on the query string or in the request body").build();
+        } else {
+            message.setValue(content);
+
+            return request.createResponseBuilder(HttpStatus.OK).body("Message sent to service-bus").build();
         }
-
-        return request.createResponseBuilder(HttpStatus.OK).body("Message sent to Service Bus.").build();
     }
+
 }
