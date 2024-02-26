1edac3157332d89fac7c4551d3ed2b0fefa748df
Benedict Chng <benedict.chng@coinvest.com.au>
2024-02-26 16:58:47 +1100

    Working version for swagger.json generation!

--- src/main/java/au/com/coinvest/function/AnnotatedHttpTrigger.java
+++ src/main/java/au/com/coinvest/function/AnnotatedHttpTrigger.java
@@ -2,6 +2,8 @@ package au.com.coinvest.function;
 
 import java.util.Optional;
 
+import javax.ws.rs.Path;
+
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -13,31 +15,28 @@ import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import au.com.coinvest.domain.Customer;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
+@Path("/Adjustment")
+@Api(value="AdjustmentResource for Adjustment")
 public class AnnotatedHttpTrigger {
 
     @FunctionName("ProcessCustomer")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Customer processed successfully"),
-        @ApiResponse(responseCode = "400", description = "Bad request"),
-        @ApiResponse(responseCode = "500", description = "Internal server error")
+        @ApiResponse(code = 200, message = "Customer processed successfully", response = Customer.class),
+        @ApiResponse(code = 400, message = "Bad request"),
+        @ApiResponse(code = 500, message = "Internal server error")
     })
-    @Operation(summary = "Process a customer", description = "Process a customer with specified parameters")
     public HttpResponseMessage processCustomer(
         @HttpTrigger(name = "req", methods = {HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS)
         HttpRequestMessage<Optional<Customer>> request,
-        @Parameter(name = "param1", in = ParameterIn.QUERY, required = true, description = "Query parameter 1")
         @BindingName("param1")
         String param1,
-        @Parameter(name = "param2", in = ParameterIn.QUERY, required = true, description = "Query parameter 2")
         @BindingName("param2")
         int param2,
         ExecutionContext context
